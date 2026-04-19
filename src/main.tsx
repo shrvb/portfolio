f55b79c879f7a2b7074bf68b0e99b17b4fcee423
Shravan <heyshrav@gmail.com>
2026-04-19 11:29:26 +0000

    added tailwind & vercel stats scripts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )

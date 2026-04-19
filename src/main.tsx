d7be9dd368163530993c0f35f056ea54d15f7042
Shravan <heyshrav@gmail.com>
2026-04-19 11:30:54 +0000

    fixed import ref

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
